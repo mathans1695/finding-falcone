e91f4c4fb9d4ab0a70d58177e0eb4dfe271a77a9
Mahalingam Sundararaj <mathans1695@gmail.com>
2020-10-26 17:49:33 +0530

    request for planets and vehicles, send the response to Falcone component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
+import React, { Component } from 'react';
+import Falcone from './components/Falcone';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			planets: '',
+			vehicles: '',
+		}
+	}
+	
+	componentDidMount() {
+		const planetsURL = "https://findfalcone.herokuapp.com/planets",
+			  vehiclesURL = "https://findfalcone.herokuapp.com/vehicles",
+			  planetsRes = fetch(planetsURL),
+			  vehiclesRes = fetch(vehiclesURL);
+			  
+		planetsRes
+			.then(res => res.json())
+			.then(json => {
+				this.setState({planets : json});
+				return json;
+			})
+			.catch(err => console.log(err))
+		
+		vehiclesRes
+			.then(res => res.json())
+			.then(json => {
+				this.setState({vehicles : json});
+				return json;
+			})
+			.catch(err => console.log(err))
+	}
+	
+	render() {
+		const { planets, vehicles } = this.state;
+		
+		return (
+			<div className="App">
+				{planets && vehicles
+				 && <Falcone planets={planets} vehicles={vehicles} />
+				}
+			</div>
+		);
+	}
 }
 
 export default App;
