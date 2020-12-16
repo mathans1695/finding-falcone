698c68f7106ec374cf4d3630121d48a01e919fd0
Mahalingam Sundararaj <mathans1695@gmail.com>
2020-12-16 20:44:01 +0530

    remove unneccesary things from app test helper

--- src/components/App/__test__/helper.js
+++ src/components/App/__test__/helper.js
@@ -1,4 +1,1 @@
-export const mockHandleResult = (selectedPlanets, selectedVehicles) => undefined;
-
 export const selectedPlanets = ['Donlon', 'Enchai', 'Sapir', 'Pingasor'];
-export const selectedVehicles = ['Space pod', 'Space rocket', 'Space ship', 'Space ship'];
